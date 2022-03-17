7e62d0e99cb1fa4a01ddf1097cc5121905231ce1
talasandras <talasandris@gmail.com>
2022-03-17 10:40:50 +0100

    0317_talasa

--- cellanalysis/cellanalysis.py
+++ cellanalysis/cellanalysis.py
@@ -2,26 +2,30 @@
 from skimage import io # conda install scikit-image
 from aicsimageio import AICSImage  # pip install aicsimageio és pip install aicspylibczi
 
-
 class Image:
     """basic image class which stores the image metadata:
             - image location
             - image type
             - channel info
             - etc."""
-
-    def __init__(self):
+    
+    def __init__(self, path):
         # or __post_init__
         # definiáljuk a self.image_path paramétert!
         # self.channel_number
         # self.nucleus_channel 
-
+        self.image_path = path
+    
     def load_image(self):
         """loads the image data and stores it in self.image"""
         
         # io.imread funkció vagy AICSImage(path)
         # használjuk a self.image_path paramétert!
-        
+        path_end = self.image_path[-4:]
+        if path_end == '.tif':
+            self.image = io.imread(self.image_path)
+        elif path_end == '.czi':
+            self.image = AICSImage(self.image_path).data
 
     def display_image(self):
         """displays the image channels on a pyplot figure"""
